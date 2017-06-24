207bf012306d108adb52b2a0b1b2d615f5035fce
Gabriel Bernal <gabrielbernalp@gmail.com>
2017-06-24 13:04:02 -0500

    Exported configuration interface

--- src/lizard-angular-sdk.ts
+++ src/lizard-angular-sdk.ts
@@ -4,5 +4,6 @@ export { AuthService } from './common/auth.service';
 export { APIService } from './common/api.service';
 
 export { UsersService } from './resources/users.service';
+export { ILizardConfig } from './common/lizard-config.interface';
 
 export { LizardSDKModule } from './modules/lizard-sdk.module';
